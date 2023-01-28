b5d449ba9231fe2be733f16faed75d454507b340
Yokesh <yoksyokesh@gmail.com>
2023-01-28 21:59:55 +0530

    WIP

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { NativeBaseProvider, Box } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { LoginScreen } from "./screens";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <NativeBaseProvider>
-      <Box safeArea>Hello world</Box>
+      <Box safeArea flex={1}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Box>
     </NativeBaseProvider>
   );
 }
-
---
+++ screens/Login.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import { Box, Text } from "native-base";
+
+const Login = () => {
+  return (
+    <Box bgColor={"#ffffff"} flex={1} alignItems="center">
+      <Text>Login screen...</Text>
+    </Box>
+  );
+};
+
+export default Login;
